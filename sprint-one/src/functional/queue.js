ba85da8d2bcdbc52df9766442dd692d44fd0f848
Marissa <marissa.shieh@gmail.com>
2019-02-20 12:46:15 -0800

    Functional queue implemented
    Storage is a private variable. Lets you enqueue, dequeue, and find size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,40 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var index = -1;
+  var length = index + 1;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    index = index + 1;
+    storage[index] = value;
+    length = index + 1;
+    console.log(storage);
   };
 
   someInstance.dequeue = function() {
+    var value;
+    if (length>1){
+      value = storage[0];
+      delete storage[0];
+      for(let key in storage){
+        storage[key-1] = storage[key];
+      }
+      delete storage[index];
+      length = length - 1;
+      index = index - 1;
+    } else if (length === 1){
+      value = storage[0];
+      delete storage[0];
+      length = length - 1;
+      index = index - 1;
+    }
+    return value;
+     console.log(storage);
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
