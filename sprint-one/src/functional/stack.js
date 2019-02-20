4fccec67f882aca953755ca3a4b90653c8e51812
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-20 15:07:15 -0800

    [When this code is committed it will]
    Create a Stack class which can have values pushed in, popped out, and a size property

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
+   
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return undefined;
+    } 
+    
+    var data = storage[count-1]; //the element at the top of the stack
+    count--;
+    delete storage[count];
+    return data;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+
