5314b56036c353d57743258994adfdf282cd18c2
Marissa <marissa.shieh@gmail.com>
2019-02-20 12:58:57 -0800

    Implementation of functional stack
    Quite similiar to functional queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = -1;
+  var length = index + 1;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    index = index + 1;
+    storage[index] = value;
+    length = length + 1;
   };
 
   someInstance.pop = function() {
+    if (length>=1){
+    var value = storage[index];
+    delete storage[index];
+    index = index - 1;
+    length = length - 1;
+    return value;
+    }
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
