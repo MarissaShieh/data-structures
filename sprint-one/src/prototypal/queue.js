b1619de6cb273555455782a7ae18fb9f236dc0db
Marissa <marissa.shieh@gmail.com>
2019-02-20 21:45:42 -0800

    Implemented Prototypal Queue
    Same as Prototypal Stack. Uses queueMethods because the test requires it

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.index = -1;
+  someInstance.length = someInstance.index + 1;
+  return someInstance;
 };
-
 var queueMethods = {};
-
-
+queueMethods.enqueue = function(value) {
+  this.index = this.index + 1;
+  this.storage[this.index] = value;
+  this.length = this.length + 1;
+};
+queueMethods.dequeue = function() {
+  var value = this.storage[0];
+  if (this.length >= 1) {
+    for (let key in this.storage) {
+      if (parseInt(key)) {
+        key = parseInt(key);
+        this.storage[key - 1] = this.storage[key];
+      } 
+    }
+    delete this.storage[-1];
+    delete this.storage[this.index];
+    this.index = this.index - 1;
+    this.length = this.length - 1;
+    return value; 
+  }
+};
+queueMethods.size = function() {
+  return this.length;
+};
