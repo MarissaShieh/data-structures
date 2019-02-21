49baf3b46b533e2943b9c75b4fef662a37e7f38e
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-20 21:50:27 -0800

    [When this code is committed it will]
    Create an instance of a stack class which has a storage object, a count, and several methods including push pop and size. Using functional shared method.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }, 
+  pop: function() {
+    if (this.count === 0) {
+      return undefined;
+    }  
+    this.count--;
+    return this.storage[this.count];
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
