3d1fd003320853a0f884f827fcf20a77d898e27a
Marissa <marissa.shieh@gmail.com>
2019-02-20 17:36:58 -0800

    Stack function-shared implemented
    Turns index, length, and storage into keys and values within someInstance. This may make functional-shared Queue a problem/

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.index = -1;
+  someInstance.length = someInstance.index + 1;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
-
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.index = this.index + 1;
+    this.storage[this.index] = value;
+    this.length = this.length + 1;
+  }, pop: function() {
+    if (this.length >= 1){
+      var value = this.storage[this.index];
+      delete this.storage[this.index];
+      this.index = this.index - 1;
+      this.length = this.length - 1;
+      return value;
+    }
+  }, size: function() {
+    return this.length;
+  }
+};
