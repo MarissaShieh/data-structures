e5afe1f4c4ea62b20b68839bc1a9cf8ee0d88396
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-20 22:33:25 -0800

    [When this code is committed it will]
    it works ! with ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
+
+    enqueue (value) {
+      this.storage[this.count] = value;
+      this.count++;
+    }
+
+    dequeue () {
+      var data = this.storage[0];
+
+      if (this.count === 0) {
+        return undefined;
+      }
+      
+      for (var kk = 0; kk < this.count; kk++) {
+          this.storage[kk] = this.storage[kk+1];
+      }
+      this.count--; 
+      delete this.storage[this.count];
+  
+      return data;
+    }
+    
+    size () {
+      return this.count;
+    }
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    } 
+    
+    var data = this.storage[this.count-1]; //the element at the top of the stack
+    this.count--;
+    delete this.storage[this.count];
+    return data;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  
 }
