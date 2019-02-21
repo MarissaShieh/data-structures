4cd0920e4466f1f09f582548b5aa48b2580bdcff
Marissa <marissa.shieh@gmail.com>
2019-02-20 21:43:48 -0800

    Implemented Prototypal stack
    Uses stackMethods instead of Stack.prototype because it's what the test called for. Also does not use Stack.stackMethods because the test is looking specifically for stackMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,80 @@
+// var Stack = function() {
+// // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// // but try not not reference your old code in writing the new style.
+//   var someInstance = Object.create(Stack.prototype);
+//   someInstance.storage = {};
+//   someInstance.index = -1;
+//   someInstance.length = someInstance.index + 1;
+//   return someInstance;
+// };
+// Stack.prototype.push = function(value) {
+//   this.index = this.index + 1;
+//   this.storage[this.index] = value;
+//   this.length = this.length + 1;
+// };
+// Stack.prototype.pop = function() {
+//   if (this.length >= 1){
+//     var value = this.storage[this.index];
+//     delete this.storage[this.index];
+//     this.index = this.index - 1;
+//     this.length = this.length - 1;
+//     return value;
+//   }
+// };
+// Stack.prototype.size = function() {
+//   return this.length;
+// };
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.index = -1;
+  someInstance.length = someInstance.index + 1;
+  return someInstance;
+};
+var stackMethods = {
+  push: function(value) {
+    this.index = this.index + 1;
+    this.storage[this.index] = value;
+    this.length = this.length + 1;
+  }, pop: function() {
+    if (this.length >= 1){
+      var value = this.storage[this.index];
+      delete this.storage[this.index];
+      this.index = this.index - 1;
+      this.length = this.length - 1;
+      return value;
+    }
+  }, size: function() {
+    return this.length;
+  }
 };
 
-var stackMethods = {};
-
-
+// var Stack = function() {
+// // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// // but try not not reference your old code in writing the new style.
+//   var someInstance = Object.create(Stack.stackMethods);
+//   someInstance.storage = {};
+//   someInstance.index = -1;
+//   someInstance.length = someInstance.index + 1;
+//   return someInstance;
+// };
+// Stack.stackMethods = {
+//   push: function(value) {
+//     this.index = this.index + 1;
+//     this.storage[this.index] = value;
+//     this.length = this.length + 1;
+//   }, pop: function() {
+//     if (this.length >= 1){
+//       var value = this.storage[this.index];
+//       delete this.storage[this.index];
+//       this.index = this.index - 1;
+//       this.length = this.length - 1;
+//       return value;
+//     }
+//   }, size: function() {
+//     return this.length;
+//   }
+// };
