fcadd195a59738d2a0ed68de176ddf352afd633f
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-20 21:35:00 -0800

    [When this code is committed it will]
    Make a queue constructor that has storage, count, and methods like enqueue, dequeue, and size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+
+  dequeue: function() {
+    var data = this.storage[0];
+    
+    if (this.count === 0) {
+      return undefined;
+    }
+    for (var kk = 0; kk < this.count; kk++) {
+      this.storage[kk] = this.storage[kk+1];
+    }
+    this.count--;
+    delete this.storage[this.count];
+
+    return data; 
+  }, 
+
+  size: function() {
+    return this.count;
+  }
 
 
+};
+
+
+//for functional shared, we use these keywords. We still create an empty object, return it. 
+//this
+//extend
+
+//you will defined your object as an empty object (someInstance)
+//then put all your object methods in another object 
