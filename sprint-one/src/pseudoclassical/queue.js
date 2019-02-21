f92fe8f52bd2c0af90750ff13c88c7c05a37f221
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-02-20 22:02:25 -0800

    [When this code is committed it will]
    Implement stack and queue constructors using pseudoclassical (instead of stackMethods or queueMethods, we use prototype as the object where we store our methods). No someInstance object is created.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  this.storage = {};
+  this.count = 0;
+
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+  var data = this.storage[0];
+  
+  if (this.count === 0) {
+    return undefined;
+  }
+  for (var kk = 0; kk < this.count; kk++) {
+    this.storage[kk] = this.storage[kk+1];
+  }
+  this.count--;
+  delete this.storage[this.count];
+
+  return data; 
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
