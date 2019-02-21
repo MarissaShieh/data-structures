52780a09da363c029c1d8ef2becb71b35161c534
MarissaShieh <marissa.shieh@gmail.com>
2019-02-21 11:26:09 -0800

    Implemented addToTail, started removeHead, IOCE whiteboarding for contains()

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,24 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    newNode.next = null; //our new tail
+    list.tail = newNode; //our new tail
+    if (list.head === null) {
+      list.head = newNode;    
+    }
   };
 
   list.removeHead = function() {
+    var removedNode = list.head;
+    list.head = removedNode.next; //move nametag head to next node
+    removedNode.next = null;
+    return removedNode.value;   
   };
 
   list.contains = function(target) {
+    
+    
   };
 
   return list;
@@ -26,4 +38,19 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * addToTail - O(1) constant because you have a reference to the tail 
+ * (the nametag), only one operation is required per input. 
+ * 
+ * removeHead - O(1) constant time, same reason as tail (except head nametag)
+ *
+ * contains - O(n) linear time complexity, because you have a lookup loop.
  */
+
+// input -- target will be anything, a string number (assume not array/obj), could be boolean
+// output -- boolean
+// constraints -- assume target is simple
+// edges cases -- empty list
+
+// Justificiation: function will compare until it finds target value is node's value
+
