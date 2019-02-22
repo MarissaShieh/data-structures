485b1c12a17ecc775736cd6755118691d96a5a58
MarissaShieh <marissa.shieh@gmail.com>
2019-02-22 14:11:03 -0800

    Attempted Tree method contains
    When tree contains target, correctly returns "true". However,
    when the tree does not contain the target value, it does not handle the
    bottom-most children correctly

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,6 +3,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+//newTree.addTree = function () { this.}
+
   // your code here
   newTree.children = [];
   _.extend(newTree, treeMethods);
@@ -12,15 +14,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  var leaf = new Tree(value); //new?
+  var leaf = Tree(value);
   this.children.push(leaf);
 };
 
 treeMethods.contains = function(target) {
+  var isFound = false;
+  if(target === this.value) {
+    return true;
+  } else {
+    if(this.children === []) {
+      isFound = false;   
+    } else {
+      for(let i = 0; i<this.children.length; i++) {
+        return this.children[i].contains(target);
+      }
+    }
+  }
+  return isFound;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
  */
