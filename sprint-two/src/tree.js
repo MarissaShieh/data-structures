f5205fbfecc716cc157730ae0cbc7fed752d6ca7
MarissaShieh <marissa.shieh@gmail.com>
2019-02-22 15:20:34 -0800

    Added answer to Time Complexity for Tree add and contains.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,6 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-//newTree.addTree = function () { this.}
-
   // your code here
   newTree.children = [];
   _.extend(newTree, treeMethods);
@@ -23,14 +21,12 @@ treeMethods.contains = function(target) {
   if(target === this.value){
     return true;
   }
-  
   if(this.children.length===0) {
     return false;   
   } 
   for(let i = 0; i<this.children.length; i++) {
     isFound = isFound || this.children[i].contains(target);
   }
-  
   return isFound;
 };
 
@@ -38,5 +34,13 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
+ * .addChild() has O(1) constant time complexity because for each input value
+ * you do a constant number of operations. Creating a new node is O(1). Pushing
+ * to the children array is also O(1).
+ * 
+ *  .contains() has O(n) linear time complexity because a for loop has O(n).
+ *  Recursion adds execution contexts, but doesn't increase the order of magnitude.
+ *  It will only 2n 4n 10n
  * 
  */
